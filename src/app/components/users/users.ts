95f44a819c55387d3f43e0166b7d032eba7c2e18
meanochi <michal.icecream@gmail.com>
2026-02-15 19:38:40 +0200

    fix (server side also) signup and login

--- src/app/components/users/users.ts
+++ src/app/components/users/users.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Login } from './login/login';
 import { Signup } from "./signup/signup";
+import { InplaceModule } from 'primeng/inplace';
 
 @Component({
   selector: 'app-users',
-  imports: [Login, Signup],
+  imports: [Login, Signup,InplaceModule],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
